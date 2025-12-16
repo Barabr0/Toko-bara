d639bd7233a10b994ae4461104f970eaa3021630
Barabr0 <www.marsha698@gmail.com>
2025-12-16 15:48:55 +0700

    day 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,34 +15,77 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     *
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'avatar',
+        'google_id',
+        'phone',
+        'address',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     *
      */
     protected $hidden = [
         'password',
         'remember_token',
     ];
+    protected function casts() : array {
+        return
+        [
+            'email_veryfied_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+   }
+
+   public function cart()
+    {
+        return $this->hasOne(Cart::class);
+   }
+   public function wishlists()
+    {
+        return $this->hasMany(Wishlist::class);
+   }
+
+   public function orders()
+    {
+        return $this->hasMany(Order::class);
+   }
+
+      public function wishlistProducts()
+    {
+        return $this->belongsToMany(Product::class, 'wishlists')
+                    ->withTimestamps();
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isCustomer(): bool
+    {
+        return $this->role === 'customer';
+    }
+
+      public function hasInWishlist(Product $product): bool
+    {
+        return $this->wishlists()
+                    ->where('product_id', $product->id)
+                    ->exists();
+    }
 
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+
 }
